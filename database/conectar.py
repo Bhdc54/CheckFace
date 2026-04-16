288b3b82c01e2bdba2ca14471986729e44636e85
Brunno Camargo <hbrunno54@gmail.com>
2026-04-15 21:03:24 -0400

    alterar banco

--- database/conectar.py
+++ database/conectar.py
@@ -9,6 +9,7 @@ def conectar():
         host=os.getenv("DB_HOST"),
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+        password=os.getenv("DB_PASSWORD"),
+        port=int(os.getenv("DB_PORT", 5432))
     )
     return conn
